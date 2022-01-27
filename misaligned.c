c3241a0f13bda68d6ddc2e4e31fe873c6cf1de79
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-01-27 15:00:22 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
+#define MAX_NO_OF_PRIMARYCOLORS  5
+const char* majorColor[MAX_NO_OF_PRIMARYCOLORS] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[MAX_NO_OF_PRIMARYCOLORS] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int CountofColorCodesPrinted= 0;
+
+struct colorcode_s
+{
+    char majorColor[25];
+    char minorColor[25];  
+    char colorCode;
+};
+
+colorcode_s ColorCodeParserStub(int majorColorIndex, int minorColorIndex);
+void printColourCodeManualInConsoleStub(int majorColorIndex, int minorColorIndex, colorcode_s colorcode);
+int printColorMapStub(void (*printColourCodeManualInConsole_FnPtr)(int,int,colorcode_s ));
+ 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -13,9 +30,55 @@ int printColorMap() {
     return i * j;
 }
 
+colorcode_s ColorCodeParserStub(int majorColorIndex, int minorColorIndex)
+{
+    colorcode_s colorcode;
+    strcpy(colorcode.majorColor, majorColor[majorColorIndex]);
+    strcpy(colorcode.minorColor, minorColor[minorColorIndex]);
+    colorcode.colorCode = (MAX_NO_OF_PRIMARYCOLORS * majorColorIndex) + minorColorIndex + 1;
+    return colorcode;   
+}
+
+/* Stub code to use in test enviroment, in production actual console interfaces can be called */
+void printColourCodeManualInConsoleStub(int majorColorIndex, int minorColorIndex, colorcode_s colorcode)
+{
+    printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, colorcode.majorColor, colorcode.minorColor);
+    CountofColorCodesPrinted++;
+}
+
+int printColorMapStub(void (*printColourCodeManualInConsole_FnPtr)(int,int,colorcode_s ))
+{
+    colorcode_s colorcode;
+    int majorColorIndex = 0; 
+    int minorColorIndex = 0;
+
+    for(majorColorIndex = 0; majorColorIndex < MAX_NO_OF_PRIMARYCOLORS; majorColorIndex++) 
+    {
+        for(minorColorIndex = 0; minorColorIndex < MAX_NO_OF_PRIMARYCOLORS; minorColorIndex++) 
+        {
+            colorcode = ColorCodeParserStub(majorColorIndex,minorColorIndex);
+            printColourCodeManualInConsole_FnPtr(majorColorIndex,minorColorIndex,colorcode);
+        }
+    }
+
+    return (minorColorIndex * majorColorIndex);
+}
+
+
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    colorcode_s colorcode;
+    int majorColorIndex = 0; 
+    int minorColorIndex = 0;
+    int CountofColorCodesPrinted_expected = 0;
+
+    void (*printColourCodeManualInConsole_FnPtr)(int,int,colorcode_s) = printColourCodeManualInConsoleStub;
+    /*Test ColorCodeParserStub */
+    colorcode = ColorCodeParserStub(majorColorIndex,minorColorIndex);
+    assert(colorcode.colorCode == (majorColorIndex * 5 + minorColorIndex));
+    /*Test printColorMapStub */
+    CountofColorCodesPrinted_expected = printColorMapStub(printColourCodeManualInConsole_FnPtr);
+    assert(CountofColorCodesPrinted_expected == CountofColorCodesPrinted);
+    CountofColorCodesPrinted = 0;
     printf("All is well (maybe!)\n");
     return 0;
 }
